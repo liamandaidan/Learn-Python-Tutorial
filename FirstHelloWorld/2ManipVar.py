1e04f2c3403057dbe4409956850359fa66c5e4cb
Liam McLaughlin <84680661+liamandaidan@users.noreply.github.com>
2022-02-17 15:55:56 -0700

    manip vars

---
+++ FirstHelloWorld/2ManipVar.py
@@ -0,0 +1,24 @@
+#Lets start with declaring some variables to work with
+x = 1
+y = 2
+z = 4
+#lets begin with some basic addition
+print(x+y)
+#The output of x+y is 3. Or in other words..
+# 1 + 2 = 3
+
+#Lets say we want to store the output into a variable
+output = x+y
+#Now we have assiged output with the values of 1+2
+print(output)
+#output is now: 3
+
+#With variables we can assign different operators between them
+# +, -, *, /, % addition, subtraction, multiplication, division, and modulus
+print(y-x)
+print(y*y)
+print(z/y)
+#mod operation
+#TLDR acts like divsion, returns remainder.
+print(10%3)
+#10/3 leaves a 1 remainder..
