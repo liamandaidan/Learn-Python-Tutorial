ad76280f5df8454ef8a8fc203590983ac30be273
Liam McLaughlin <84680661+liamandaidan@users.noreply.github.com>
2022-02-17 15:23:51 -0700

    Vars and types

---
+++ FirstHelloWorld/1VariablesAndTypes.py
@@ -0,0 +1,24 @@
+#integer(Whole digits)
+int = 7
+print(int)
+
+#float(Decimal digits)
+floatOne = 2.0
+print(floatOne)
+#OR
+floatTwo = float(2)
+print(floatTwo)
+
+"""
+Strings(Character sequences)
+Note with strings that you can have content with either "" or ''
+string = 'hello123!'
+string = "hello123!"
+With "" it means that an input can contain ' inside of them
+"""
+string = "hello123!"
+print(string)
+
+#boolean(True/False type)
+bool = True
+print(bool)
